40839d3a35cb8f85428322349b4c94216f2a3112
Kevin Suen <kevinsuen11@gmail.com>
2019-08-21 19:02:05 +0000

    Add node-postgress examples

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,34 @@
+const { Pool } = require('pg');
+
+const args = process.argv.slice(2);
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.connect();
+
+pool
+  .query(
+    `
+    SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+    FROM teachers
+    JOIN assistance_requests ON teachers.id = teacher_id
+    JOIN students ON students.id = student_id
+    JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name LIKE '%${args[0]}%'
+    ORDER BY teachers.name;
+    `
+  )
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(
+        `${user.cohort}: ${user.teacher}`
+      );
+    });
+  }).catch(err => console.error('query error', err.stack));
+
+
