5f889dd93414969d08c16fc18d80fe223da8b13a
Kevin Suen <kevinsuen11@gmail.com>
2019-08-21 19:44:42 +0000

    Update for sql injection prevention

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -43,16 +43,19 @@ pool
     });
   });
 
+
+const text =     
+  `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const values = [`%${args[0]}%`, `${args[1] || 5}`];
+
 pool
-  .query(
-    `
-    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${args[0]}%'
-    LIMIT ${args[1] || 5};
-    `
-  )
+  .query(text, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,18 +11,20 @@ const pool = new Pool({
 
 pool.connect();
 
+const text =
+  `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name;
+  `;
+const values = [`%${args[0]}%`];
+
 pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-    FROM teachers
-    JOIN assistance_requests ON teachers.id = teacher_id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${args[0]}%'
-    ORDER BY teachers.name;
-    `
-  )
+  .query(text, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(
